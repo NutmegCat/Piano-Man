f6aca03f1d2602fb8b6da809d75ff6f95bfd7a9d
NutmegCat <deltaechogames@gmail.com>
2024-10-11 12:31:09 -0400

    first commit

--- Piano_Music.m
+++ Piano_Music.m
@@ -1,33 +1,10 @@
-clear
-clc
-
-a = arduino('COM5', 'Nano3'); % or use COM port
-max_samples = 300; % how many times do we check the sound sensor?
-filter_size = 5;
-tic % start timer
-
-sound_data = zeros(1, max_samples); % preallocate arrays for speed
-time_data = zeros(1, max_samples);
-avg_sound_data = zeros(1, max_samples - filter_size);
-
-figure; % Create a new figure for plotting
-hPlot = plot(nan, nan); % Initialize the plot with empty data
-xlabel('Time (s)');
-ylabel('Average Sound Voltage');
-title('Live Sound Data Plot');
-xlim([0, max_samples * 0.1]); % Adjust x-axis limits as necessary
-ylim([0, 5]); % Adjust y-axis limits according to your sensor voltage range (0-5V)
-
+clear all; close all;
+a = arduino('/dev/tty.SLAB_USBtoUART', 'uno') % or use COM port
+tic %start timer
+max_samples = 1000; % how many times do we check the sound sensor?
+tic %start timer
 for i = 1:max_samples
-    sound_data(i) = readVoltage(a, 'A2');
+    sound_data(i) = readVoltage(a,'A2')
     time_data(i) = toc;
-
-    % Only calculate moving average once we have enough samples
-    if i > filter_size
-        avg_sound_data(i - filter_size) = mean(sound_data(i-filter_size+1:i));
-        
-        % Update the plot with new data points
-        set(hPlot, 'XData', time_data(filter_size+1:i), 'YData', avg_sound_data(1:i-filter_size));
-        drawnow; % Force MATLAB to update the plot immediately
-    end
 end
+plot(time_data, sound_data)
