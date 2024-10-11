bca777f90314447d294b3b4052529c1016714d27
NutmegCat <deltaechogames@gmail.com>
2024-10-11 12:22:48 -0400

    Part 2 Completed

--- Piano_Music.m
+++ Piano_Music.m
@@ -2,15 +2,24 @@ clear
 clc
 
 a = arduino('COM5', 'Nano3'); % or use COM port
-tic %start timer
 max_samples = 300; % how many times do we check the sound sensor?
 filter_size = 5;
 tic %start timer
 
-for i = 1:(max_samples-filter_size)
-    sound_data(i) = readVoltage(a,'A2');
+sound_data = zeros(1, max_samples); % preallocate arrays for speed
+time_data = zeros(1, max_samples);
+avg_sound_data = zeros(1, max_samples - filter_size);
+
+for i = 1:max_samples
+    sound_data(i) = readVoltage(a, 'A2');
     time_data(i) = toc;
-    avg_sound_data(i) = mean(sound_data(i:i+filter_size));
+
+    % Only calculate moving average once we have enough samples
+    if i > filter_size
+        avg_sound_data(i - filter_size) = mean(sound_data(i-filter_size+1:i));
+    end
 end
 
-plot(time_data, avg_sound_data)
+plot(time_data(filter_size+1:end), avg_sound_data);
+xlabel('Time (s)');
+ylabel('Average Sound Voltage');
